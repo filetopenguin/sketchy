29b09f4a58f69f865e11f4d7d5941f62e7e44a81
filetopenguin <arielweiser17@gmail.com>
2023-11-21 00:50:24 +0200

    added a random color generator

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
 const grid = document.querySelector('.grid');
 const reset = document.querySelector('.reset');
+const colors = document.querySelector('.colors');
+const randomc = document.querySelector('.randomc');
 
+randomc.addEventListener("click",randomColor);
 reset.addEventListener("click",resetGrid);
 
+function randomColor(){
+    const boxes = grid.querySelectorAll('.box');
+    boxes.forEach(box=>box.addEventListener('mouseover',()=>{
+        box.style.background=getRandomColor();
+    }))
+}
+function getRandomColor() {
+    // Generate a random color in the format #RRGGBB
+    return `#${Math.floor(Math.random()*16777215).toString(16)}`;
+}
 function setGrid(size){
     for(let i=0; i< (size*size); i++){
         const box = document.createElement('div');
